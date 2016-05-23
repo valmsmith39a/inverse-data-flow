cb94afa0ace595a2c6a82797fc4c09acce891000
valmsmith39a <valmsmith39a@gmail.com>
2016-05-22 17:37:27 -0700

    creates and wires up parent component

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,1 +1,9 @@
 console.log('hello from index.js')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Parent from './parent'
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+)
